7d0235807a8024137b0fc840833d45add37ba35d
SAJL <sarah_ledoux@harvard.edu>
2014-10-11 06:47:08 -0400

    updated server identification in footer call

--- p2-resubmit/includes/footer.php
+++ p2-resubmit/includes/footer.php
@@ -2,7 +2,7 @@
         <footer class="row">
                	<p><span class="glyphicon glyphicon-copyright-mark"></span> 2014 SAJL</p>
 				<?php  
-    			ECHO 'The host server is morpheus.dce.harvard.edu and the path is ' . $_SERVER['REQUEST_URI'];
+    			ECHO 'The host server is sarahledoux.com and the path is ' . $_SERVER['REQUEST_URI'];
     			?>
         </footer>
 <!--end footer-->
